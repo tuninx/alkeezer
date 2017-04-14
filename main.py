8e5ff986cfa2f727ad6c867f3cb2d1355206db6a
tuninx <tuninx@gmail.com>
2017-04-14 20:24:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -205,9 +205,9 @@ def parse_text(text, username, message_id):
             hero_message_id = message_id
             m = re.search('Битва пяти замков через(?: ([0-9]+)ч){0,1}(?: ([0-9]+)){0,1}', text)
             if not m.group(1):
-                if m.group(2) and int(m.group(2)) <= 59:
+                if m.group(2) and int(m.group(2)) <= 20:
                     state = re.search('Состояние:\\n(.*)$', text)
-                    if auto_def_enabled and time() - current_order['time'] > 3600:
+                    if auto_def_enabled and time() - current_order['time'] > 1080:
                         if donate_enabled:
                             gold = int(re.search('💰([0-9]+)', text).group(1))
                             if gold > gold_to_left:
